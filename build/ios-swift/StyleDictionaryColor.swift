b94c81e5bdee479b9724eb60f8a922453bb616de
aali-submittable <aali-submittable@users.noreply.github.com>
2022-12-28 23:49:08 +0000

    Tokens updated

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,12 +4,12 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 27 Dec 2022 01:28:40 GMT
+// Generated on Wed, 28 Dec 2022 23:49:08 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let primarycolor = UIColor(red: 0.882, green: 0.118, blue: 0.349, alpha: 1)
+    public static let primarycolor = UIColor(red: 0.208, green: 0.145, blue: 0.929, alpha: 1)
     public static let secondarycolor = UIColor(red: 0.820, green: 0.671, blue: 0.447, alpha: 1)
 }
